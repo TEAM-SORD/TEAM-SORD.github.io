455a68ee75c41778fbe484d7c3b13f5836cb8583
beechware <davebeechmovement@yahoo.co.uk>
2015-01-30 00:32:05 +0000

    please stick

--- parallax.js
+++ parallax.js
@@ -13,3 +13,15 @@ $(document).ready(function(){
    });
     
 });
+
+
+
+
+$('.growImage').mouseover(function(){
+      //moving the div left a bit is completely optional
+      //but should have the effect of growing the image from the middle.
+      $(this).stop().animate({"width": "100%","left":"0px","top":"0px"}, 400,'swing');
+    }).mouseout(function(){ 
+      $(this).stop().animate({"width": "80%","left":"15px","top":"15px"}, 200,'swing');
+    });;
+});
